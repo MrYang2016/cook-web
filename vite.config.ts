8625f3a4ab61cf692d8ac378a891050266a58899
羊锡贵 <yangxigui@corp.polyv.net>
2025-01-12 14:03:52 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, 'sitemap.xml'),
+          dest: '' // 目标目录，留空表示复制到 dist 根目录
+        }
+      ]
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
